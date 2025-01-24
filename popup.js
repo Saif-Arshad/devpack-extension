d83d0c009043f7099b01da9ea0d5056a52a09c22
saif-arshad <saifurrehman.web@gmail.com>
2025-01-24 14:51:05 +0500

    started

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const testButton = document.getElementById('testBtn');
+    testButton.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
